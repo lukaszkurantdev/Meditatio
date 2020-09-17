0a3b2204de4173aec6bc966840be6d7e2605e086
Łukasz Kurant <haluxo1@gmail.com>
2020-09-17 19:28:49 +0200

    Custom fonts.

---
+++ src/styles/Fonts.ts
@@ -0,0 +1,12 @@
+const Fonts = {
+  REGULAR: 'AgeoPersonalUse',
+  THIN: 'AgeoPersonalUse-Thin',
+  BOLD: 'AgeoPersonalUse-Bold',
+  EXTRABOLD: 'AgeoPersonalUse-ExtraBold',
+  HEAVY: 'AgeoPersonalUse-Heavy',
+  LIGHT: 'AgeoPersonalUse-Light',
+  MEDIUM: 'AgeoPersonalUse-Medium',
+  SEMIBOLD: 'AgeoPersonalUse-SemiBold',
+};
+
+export default Fonts;
